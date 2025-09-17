70b0ac3b406dc9c21fa79673ccee18c81f76798d
Saralaselvam <saralaselvam1998@gmail.com>
2025-09-17 14:09:10 +0530

    First time push

---
+++ src/test/java/com/sample/ClassA.java
@@ -0,0 +1,18 @@
+package com.sample;
+
+public class ClassA {
+	
+	public void sarala() {
+		System.out.println("Sarala");
+
+	}
+	public void gautham() {
+		System.out.println("Gautham");
+
+	}
+	public void harshitha() {
+		System.out.println("Harshitha");
+
+	}
+
+}
